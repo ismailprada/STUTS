49ad8c878c3621e4a1875f991eb4c85ea2176fa4
Samuel Läubli <laeubli@ifi.uzh.ch>
2017-05-08 10:39:57 +0200

    remove unnecessary import

--- HaeufigsteWoerter.py
+++ HaeufigsteWoerter.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import string
-
 infile = open("newstest2009.de")
 
 word_counting_dict = {}
@@ -13,7 +11,7 @@ for line in infile:
             word_counting_dict[word] += 1
         else:
             word_counting_dict[word] = 1
-            
+
 for word in sorted(word_counting_dict.items(), key=lambda x: x[1], reverse=True)[:100]:
     print(word)
 
